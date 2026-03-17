d9c695c8fbb6164cca14b801088c272aadf280f0
briantruan <86815054+briantruan@users.noreply.github.com>
2026-03-17 15:06:11 -0400

    updated way data is loaded for flexible options

--- 00_setup.r
+++ 00_setup.r
@@ -20,18 +20,10 @@ install_github("sshrestha274/meps_r_pkg/MEPS")
 
 library(MEPS)
 
-# Helper functions for loading MEPS data
-# and saving as .rda for easy access 
-# (shouldn't need to be uncommented)
-
-# Save data as .rda for easy loading
-save_rda <- function(year = year) {
-  options(survey.lonely.psu = "adjust")
-
-  # year-specific variables
-  # get last 2 digits for year
+# Load MEPS data for a single year and return a named list: fyc, cond, rx
+load_meps_year <- function(year) {
   short_year <- substr(year, 3, 4)
-  
+
   perwt  <- paste0("PERWT",  short_year, "F")
   ttlp   <- paste0("TTLP",   short_year, "X")
   povcat <- paste0("POVCAT", short_year)
@@ -45,34 +37,21 @@ save_rda <- function(year = year) {
   rxtot  <- paste0("RXTOT",  short_year)
   ertot  <- paste0("ERTOT",  short_year)
 
-  # vars to keep
-  # outcome: number of ADHD-related rx fills per person-year
-  # explanatory: post-covid binary indicator
+  vars_to_keep <- c("DUPERSID","PANEL","VARSTR","VARPSU", perwt, ttlp,
+                    "SEX", "AGE53X", "RACETHX", "MARRY53X", "REGION53",
+                    "EDUYRDG", "EDUCYR", povcat,
+                    "PCS42", "MCS42", "VPCS42", "VMCS42",
+                    "HAVEUS42", inscov, insurc, "MCAID53X",
+                    totslf, obtotv, optotv, ipdis, ipngtd, rxtot, ertot)
 
-  # covariates: age, sex, race/ethnicity, pov status, SES status
-  #             education, marital status, ins cov, geo region
-  #             urban/rural, has usual source of care
-  #
-  # urbanicity: cannot be accessed w/o requesting direct from AHRQ
+  cond_vars <- c("DUPERSID", "ICD10CDX", "OPNUM", "OBNUM",
+                 "IPNUM", "ERNUM", "HHNUM")
 
-  # notes
-  # insurance: can use INSCOV(YR) or INSURC(YR)
-  # more granularity: can use MCAID53X (covered by Medicaid/SCHIP)
+  rx_vars   <- c("DUPERSID", "RXBEGYRX", "RXBEGMM",
+                 "RXDRGNAM", "RXNDC", "RXDAYSUP")
 
-
-  # vars to keep
-  vars_to_keep <- c("DUPERSID","PANEL","VARSTR","VARPSU", perwt, ttlp,    # design
-                    "SEX", "AGE53X", "RACETHX", "MARRY53X", "REGION53",   # demo/ses
-                    "EDUYRDG", "EDUCYR", povcat,                          # demo/ses
-                    "PCS42", "MCS42", "VPCS42", "VMCS42",                 # SF-12
-                    "HAVEUS42", inscov, insurc, "MCAID53X",               # access
-                    totslf, obtotv, optotv, ipdis, ipngtd, rxtot, ertot)  # expenditures
-
-  # read MEPS from lib
   fyc <- read_MEPS(year = year, type = "FYC") %>%
-  
     select(any_of(vars_to_keep)) %>%
-  
     rename(
       PERWT  = perwt,
       TTLP   = ttlp,
@@ -87,33 +66,33 @@ save_rda <- function(year = year) {
       ERTOT  = ertot
     )
 
-  # vars to keep
-  cond_vars <- c("DUPERSID", "ICD10CDX", "OPNUM", "OBNUM", 
-                 "IPNUM", "ERNUM", "HHNUM")
-
-  rx_vars   <- c("DUPERSID", "RXBEGYRX", "RXBEGMM",
-                 "RXDRGNAM", "RXNDC", "RXDAYSUP")
-
   cond <- read_MEPS(year = year, type = "COND") %>%
     select(any_of(cond_vars))
 
   rx <- read_MEPS(year = year, type = "RX") %>%
     select(any_of(rx_vars))
 
-  # save as .rda for that year
-    # ensure data directory exists
-    if (!dir.exists("data")) dir.create("data", recursive = TRUE)
-
-    out_file <- file.path("data", paste0("meps_", year, ".rda"))
-  
-    save(fyc, cond, rx, file = out_file)
-
-    invisible(out_file)
+  list(fyc = fyc, cond = cond, rx = rx)
 }
 
-# # uncomment if need to update dataset; otherwise; can directly load
-# # the files in 01_eda.r and beyond
 
-# for (year in 2017:2023) {
-#   save_rda(year)
-# }
+# Save multiple years into a single RDS file. Each dataset is stored with a
+# year-suffixed name (e.g. fyc_2017, cond_2017, rx_2017) inside a single list.
+save_all_years <- function(years = 2017:2023, out_file = file.path("data", "meps_all_years.rds")) {
+  if (!dir.exists("data")) dir.create("data", recursive = TRUE)
+
+  all_data <- list()
+  for (yr in years) {
+    d <- load_meps_year(yr)
+    suffix <- paste0("_", yr)
+    all_data[[paste0("fyc", suffix)]]  <- d$fyc
+    all_data[[paste0("cond", suffix)]] <- d$cond
+    all_data[[paste0("rx", suffix)]]   <- d$rx
+  }
+
+  saveRDS(all_data, file = out_file)
+  invisible(out_file)
+}
+
+# Uncomment to run:
+# save_all_years(2017:2023)
--- 01_eda_mph.r
+++ 01_eda_mph.r
@@ -4,144 +4,115 @@
 # MPH-specific: only looking at pre-post two years, 
 # no longitudinal analyses
 
-# load 2019 data
-load("data/meps_2019.rda")
+# load rds data
+all_data <- readRDS("data/meps_all_years.rds")
+
+# merge fyc_2019 and fyc_2021 in all_data as fyc_simple
+fyc_2019 <- all_data$fyc_2019
+fyc_2021 <- all_data$fyc_2021
+cond_2019 <- all_data$cond_2019
+cond_2021 <- all_data$cond_2021
+rx_2019 <- all_data$rx_2019
+rx_2021 <- all_data$rx_2021
+
+# clear var all_data
+rm(all_data)
+gc()
 
 # convert haven labels to factors in df fyc 
 # remove preceding numbers from factor levels
-fyc <- fyc %>%
-  mutate(across(where(is.labelled), as_factor)) %>%
-  mutate(across(where(is.factor), ~ gsub("^[0-9]+ ", "", .)))
+clean_labels <- function(x) {
+  if (haven::is.labelled(x)) x <- haven::as_factor(x)
+  if (is.factor(x)) levels(x) <- gsub("^[0-9]+\\s*", "", levels(x))
+  x
+}
+
+fyc_2019 <- fyc_2019 %>% mutate(across(where(~ haven::is.labelled(.x) || is.factor(.x)), clean_labels))
+fyc_2021 <- fyc_2021 %>% mutate(across(where(~ haven::is.labelled(.x) || is.factor(.x)), clean_labels))
 
 # dollars: convert to 2021 dollars using CPI
 cpi_2019  <- 256.974
 cpi_2021  <- 278.802
 cpi_ratio <- cpi_2021 / cpi_2019
 
-fyc$income_2021 <- fyc$TTLP * cpi_ratio
-fyc$totslf_2021 <- fyc$TOTSLF * cpi_ratio
+fyc_2019$income_2021 <- fyc_2019$TTLP * cpi_ratio
+fyc_2019$totslf_2021 <- fyc_2019$TOTSLF * cpi_ratio
 
 # pcs/mcs: if not numeric, NA
-fyc$VPCS42 <- as.numeric(fyc$VPCS42)
-fyc$VMCS42 <- as.numeric(fyc$VMCS42)
+fyc_2019 <- fyc_2019 %>% mutate(across(c(VPCS42, VMCS42), ~ as.numeric(.x)))
+fyc_2021 <- fyc_2021 %>% mutate(across(c(VPCS42, VMCS42), ~ as.numeric(.x)))
 
-# eda
-summary(fyc$income_2021)
-table(fyc$SEX)
-table(fyc$RACETHX)
-table(fyc$MARRY53X)
-table(fyc$REGION53)
-table(fyc$EDUCYR)
-table(fyc$POVCAT)
-summary(fyc$VPCS42)
-summary(fyc$VMCS42)
-table(fyc$INSCOV)
-table(fyc$INSURC19)
-table(fyc$MCAID53X)
+# rename INSURC19 in fyc_2019 to INSURC
+# rename INSURC21 in fyc_2021 to INSURC
+fyc_2019 <- fyc_2019 %>% dplyr::rename(INSURC = INSURC19)
+fyc_2021 <- fyc_2021 %>% dplyr::rename(INSURC = INSURC21)
 
-# recode race/ethnicity
-fyc$ethnicity <- NA
-fyc$ethcnitiy <- ifelse(fyc$RACETHX == "HISPANIC", "Hispanic", "Non-Hispanic")
-fyc$race      <- NA
-fyc$race      <- ifelse(fyc$RACETHX == "NON-HISPANIC ASIAN ONLY", "Asian",
-                 ifelse(fyc$RACETHX == "NON-HISPANIC BLACK ONLY", "Black",
-                 ifelse(fyc$RACETHX == "NON-HISPANIC OTHER RACE OR MULTIPLE RACE", "Other",
-                 ifelse(fyc$RACETHX == "NON-HISPANIC WHITE ONLY", "White", 
-                 NA))))
-
-# recode marital status
-fyc$marital <- NA
-fyc$marital <- ifelse(fyc$MARRY53X %in% c("-1 INAPPLICABLE", "-7 REFUSED", "-8 DK"), NA,
-               ifelse(fyc$MARRY53X %in% c("MARRIED", "MARRIED IN ROUND"), "Married", 
-               "Not married"))
-
-# fix NA for region
-fyc$REGION53 <- ifelse(fyc$REGION53 == "-1 INAPPLICABLE", NA, fyc$REGION53)
-
-# recode education
-fyc$education <- NA
-fyc$education <- ifelse(fyc$EDUCYR %in% c("-1 INAPPLICABLE", "-7 REFUSED", "-8 DK", "-15 CANNOT BE COMPUTED"), NA,
-                 ifelse(fyc$EDUCYR == "NO SCHOOL/KINDERGARTEN ONLY", "Less than high school",
-                 ifelse(fyc$EDUCYR %in% c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"), "Less than high school",
-                 ifelse(fyc$EDUCYR == "GRADE 12", "High school graduation or greater",
-                 ifelse(fyc$EDUCYR %in% c("1 YEAR COLLEGE", "2 YEARS COLLEGE", "3 YEARS COLLEGE", 
-                                          "4 YEARS COLLEGE", "5+ YEARS COLLEGE"), "High school graduation or greater",
-                 NA)))))
-
-# recode insurance
-fyc$insurance <- NA
-fyc <- fyc %>%
-  mutate(
-    insurance = case_when(
-      # 1) Uninsured (any age)
-      INSURC19 %in% c("<65 UNINSURED", "65+ UNINSURED") ~ "Uninsured",
-      
-      # 2) Under 65, any private, no Medicaid
-      INSURC19 == "<65 ANY PRIVATE" & MCAID53X != "YES" ~ "Private only",
-      
-      # 3) Under 65, any private, WITH Medicaid
-      INSURC19 == "<65 ANY PRIVATE" & MCAID53X == "YES" ~ "Medicaid, with private",
-
-      # 4) Under 65, public only
-      INSURC19 == "<65 PUBLIC ONLY" & MCAID53X == "YES" ~ "Medicaid only",
-      INSURC19 == "<65 PUBLIC ONLY" & MCAID53X != "YES" ~ "Other public",
-      
-      # 5) 65+, Medicare + private (includes Medigap/MA)
-      INSURC19 == "65+ EDITED MEDICARE AND PRIVATE" & MCAID53X != "YES" ~ "Medicare, with private",
-      
-      # 6) 65+, Medicare + private + Medicaid (triple coverage)
-      INSURC19 == "65+ EDITED MEDICARE AND PRIVATE" & MCAID53X == "YES" ~ "Medicare, dual-eligible",
-      
-      # 7) 65+, Medicare only
-      INSURC19 == "65+ EDITED MEDICARE ONLY" & MCAID53X != "YES" ~ "Medicare only",
-      
-      # 8) 65+, Medicare only + Medicaid
-      INSURC19 == "65+ EDITED MEDICARE ONLY" & MCAID53X == "YES" ~ "Medicare, dual-eligible",
-      
-      # 9) 65+, Medicare + other public (no private)
-      INSURC19 == "65+ EDITED MEDICARE AND OTH PUB ONLY" & MCAID53X == "YES" ~ "Medicare, dual-eligible",
-      INSURC19 == "65+ EDITED MEDICARE AND OTH PUB ONLY" & MCAID53X != "YES" ~ "Medicare, with other public",
-      
-      # 10) 65+, no Medicare but has public/private
-      INSURC19 == "65+ NO MEDICARE AND ANY PUBLIC/PRIVATE" & MCAID53X == "YES" ~ "Medicaid, with private",
-      INSURC19 == "65+ NO MEDICARE AND ANY PUBLIC/PRIVATE" & MCAID53X != "YES" ~ "Private only",
-      
-      # Fallback: use INSCOV as a coarse backup
-      INSCOV == "UNINSURED" ~ "Uninsured",
-      INSCOV == "ANY PRIVATE" ~ "Private only",
-      INSCOV == "PUBLIC ONLY" & MCAID53X == "YES" ~ "Medicaid only",
-      INSCOV == "PUBLIC ONLY" & MCAID53X != "YES" ~ "Other public",
-      
-      TRUE ~ NA
+recode_fyc <- function(df) {
+  df %>% 
+    dplyr::mutate(
+      ethnicity = dplyr::case_when(
+        RACETHX == "HISPANIC" ~ "Hispanic",
+        TRUE ~ "Non-Hispanic"
+      ),
+      race = dplyr::case_when(
+        RACETHX == "NON-HISPANIC ASIAN ONLY" ~ "Asian",
+        RACETHX == "NON-HISPANIC BLACK ONLY" ~ "Black",
+        RACETHX == "NON-HISPANIC OTHER RACE OR MULTIPLE RACE" ~ "Other",
+        RACETHX == "NON-HISPANIC WHITE ONLY" ~ "White",
+        TRUE ~ NA_character_
+      ),
+      marital = dplyr::case_when(
+        MARRY53X %in% c("-1 INAPPLICABLE", "-7 REFUSED", "-8 DK") ~ NA_character_,
+        MARRY53X %in% c("MARRIED", "MARRIED IN ROUND") ~ "Married",
+        TRUE ~ "Not married"
+      ),
+      REGION53 = dplyr::na_if(REGION53, "-1 INAPPLICABLE"),
+      education = dplyr::case_when(
+        EDUCYR %in% c("-1 INAPPLICABLE", "-7 REFUSED", "-8 DK", "-15 CANNOT BE COMPUTED") ~ NA_character_,
+        EDUCYR %in% c("NO SCHOOL/KINDERGARTEN ONLY", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11") ~ "Less than high school",
+        EDUCYR %in% c("GRADE 12", "1 YEAR COLLEGE", "2 YEARS COLLEGE", "3 YEARS COLLEGE", "4 YEARS COLLEGE", "5+ YEARS COLLEGE") ~ "High school graduation or greater",
+        TRUE ~ NA_character_
+      ),
+      insurance = dplyr::case_when(
+        INSURC %in% c("<65 UNINSURED", "65+ UNINSURED") ~ "Uninsured",
+        INSURC == "<65 ANY PRIVATE" & MCAID53X != "YES" ~ "Private only",
+        INSURC == "<65 ANY PRIVATE" & MCAID53X == "YES" ~ "Medicaid, with private",
+        INSURC == "<65 PUBLIC ONLY" & MCAID53X == "YES" ~ "Medicaid only",
+        INSURC == "<65 PUBLIC ONLY" & MCAID53X != "YES" ~ "Other public",
+        INSURC == "65+ EDITED MEDICARE AND PRIVATE" & MCAID53X != "YES" ~ "Medicare, with private",
+        INSURC == "65+ EDITED MEDICARE AND PRIVATE" & MCAID53X == "YES" ~ "Medicare, dual-eligible",
+        INSURC == "65+ EDITED MEDICARE ONLY" & MCAID53X != "YES" ~ "Medicare only",
+        INSURC == "65+ EDITED MEDICARE ONLY" & MCAID53X == "YES" ~ "Medicare, dual-eligible",
+        INSURC == "65+ EDITED MEDICARE AND OTH PUB ONLY" & MCAID53X == "YES" ~ "Medicare, dual-eligible",
+        INSURC == "65+ EDITED MEDICARE AND OTH PUB ONLY" & MCAID53X != "YES" ~ "Medicare, with other public",
+        INSURC == "65+ NO MEDICARE AND ANY PUBLIC/PRIVATE" & MCAID53X == "YES" ~ "Medicaid, with private",
+        INSURC == "65+ NO MEDICARE AND ANY PUBLIC/PRIVATE" & MCAID53X != "YES" ~ "Private only",
+        INSCOV == "UNINSURED" ~ "Uninsured",
+        INSCOV == "ANY PRIVATE" ~ "Private only",
+        INSCOV == "PUBLIC ONLY" & MCAID53X == "YES" ~ "Medicaid only",
+        INSCOV == "PUBLIC ONLY" & MCAID53X != "YES" ~ "Other public",
+        TRUE ~ NA_character_
+      ),
+      medicaid = dplyr::case_when(
+        insurance == "Medicaid only" ~ "Medicaid only",
+        insurance == "Medicaid, with private" ~ "Medicaid, with private, non-Medicare",
+        TRUE ~ "No Medicaid"
+      ),
+      medicare = dplyr::case_when(
+        insurance %in% c("Medicare, with private", "Medicare, dual-eligible", "Medicare, with other public", "Medicare only") ~ "Medicare, any",
+        TRUE ~ "No Medicare"
+      ),
+      private_ins = dplyr::case_when(
+        insurance == "Private only" ~ "Private only",
+        TRUE ~ "Other/uninsured"
+      ),
+      has_insurance = dplyr::case_when(
+        INSCOV == "UNINSURED" ~ "No insurance",
+        INSCOV %in% c("ANY PRIVATE", "PUBLIC ONLY") ~ "Has insurance",
+        TRUE ~ NA_character_
+      )
     )
-  )
+}
 
-# Other dummies to account for insurance
-# Medicaid only, Medicaid w private, other public
-# Medicare (any), non-Medicare
-# Private
-# Uninsured
-
-fyc$medicaid <- NA
-fyc <- fyc %>% 
-  mutate(
-    medicaid = case_when(
-      insurance == "Medicaid only" ~ "Medicaid only",
-      insurance == "Medicaid, with private" ~ "Medicaid, with private, non-Medicare",
-      TRUE ~ "No Medicaid"
-    )
-  )
-
-fyc$medicare <- NA
-fyc <- fyc %>% 
-  mutate(
-    medicare = case_when(
-      insurance %in% c("Medicare, with private", "Medicare, dual-eligible", 
-                       "Medicare, with other public", "Medicare only") ~ "Medicare, any",
-      TRUE ~ "No Medicare"
-    )
-  )
-
-fyc$private_ins <- ifelse(fyc$insurance == "Private only", "Private only", "Other/uninsured")
-
-fyc$has_insurance <- ifelse(fyc$INSCOV == "UNINSURED", "No insurance", "Has insurance")
+fyc_2019 <- recode_fyc(fyc_2019)
+fyc_2021 <- recode_fyc(fyc_2021)
