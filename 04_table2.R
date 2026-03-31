95662c709f153e38032079b4aab14c2e357f4bf7
katrinahannett <257630321+katrinahannett@users.noreply.github.com>
2026-03-31 15:20:55 -0400

    table 2
    This script processes data from 2019 and 2021, filtering and mutating variables related to ADHD and demographics. It combines the datasets and creates a summary table using survey design.

--- 04_table2.R
+++ 04_table2.R
@@ -1,1 +1,118 @@
 
+library(dplyr)
+library(survey)
+library(gtsummary)
+library(stringr)
+
+options(survey.lonely.psu = "adjust")
+
+ids_both <- intersect(fyc_2019$DUPERSID, fyc_2021$DUPERSID)
+
+adhd_ids_2019 <- cond_2019 %>%
+  filter(!is.na(ICD10CDX)) %>%
+  filter(str_detect(ICD10CDX, "^F90")) %>%
+  distinct(DUPERSID) %>%
+  pull(DUPERSID)
+
+adhd_ids_2021 <- cond_2021 %>%
+  filter(!is.na(ICD10CDX)) %>%
+  filter(str_detect(ICD10CDX, "^F90")) %>%
+  distinct(DUPERSID) %>%
+  pull(DUPERSID)
+
+adhd_ids <- union(adhd_ids_2019, adhd_ids_2021)
+
+analytic_ids <- intersect(ids_both, adhd_ids)
+
+fyc_2019_sub <- fyc_2019 %>%
+  filter(DUPERSID %in% analytic_ids) %>%
+  # filter(AGE53X >= 18) %>%
+  mutate(
+    year = factor(2019),
+    sex = factor(SEX),
+    income_2021 = income_2021,
+    totslf_2021 = totslf_2021,
+    race = factor(race),
+    ethnicity = factor(ethnicity),
+    education = factor(education),
+    insurance = factor(insurance),
+    has_insurance = factor(has_insurance)
+  ) %>%
+  filter(if_all(all_of(c("DUPERSID", "VARPSU", "VARSTR", "PERWT")), ~ !is.na(.)))
+
+fyc_2021_sub <- fyc_2021 %>%
+  filter(DUPERSID %in% analytic_ids) %>%
+  # filter(AGE53X >= 18) %>%
+  mutate(
+    year = factor(2021),
+    sex = factor(SEX),
+    income_2021 = TTLP,
+    totslf_2021 = TOTSLF,
+    race = factor(race),
+    ethnicity = factor(ethnicity),
+    education = factor(education),
+    insurance = factor(insurance),
+    has_insurance = factor(has_insurance)
+  ) %>%
+  filter(if_all(all_of(c("DUPERSID", "VARPSU", "VARSTR", "PERWT")), ~ !is.na(.)))
+
+num_vars <- c(
+  "OBTOTV", "OPTOTV", "IPDIS", "IPNGTD", "RXTOT", "ERTOT",
+  "TTLP", "TOTSLF", "income_2021", "totslf_2021",
+  "AGE53X", "VPCS42", "VMCS42", "PERWT", "VARSTR", "VARPSU"
+)
+
+fyc_2019_sub <- fyc_2019_sub %>%
+  mutate(across(any_of(num_vars), ~ as.numeric(as.character(.))))
+
+fyc_2021_sub <- fyc_2021_sub %>%
+  mutate(across(any_of(num_vars), ~ as.numeric(as.character(.))))
+
+fyc_combined <- bind_rows(fyc_2019_sub, fyc_2021_sub)
+
+meps_design <- svydesign(
+  id = ~VARPSU,
+  strata = ~VARSTR,
+  weights = ~PERWT,
+  data = fyc_combined,
+  nest = TRUE
+)
+
+table1 <- tbl_svysummary(
+  meps_design,
+  by = year,
+  include = c(
+    AGE53X,
+    sex,
+    race,
+    ethnicity,
+    education,
+    has_insurance,
+    insurance,
+    income_2021,
+    totslf_2021
+  ),
+  label = list(
+    AGE53X ~ "Age, years",
+    sex ~ "Sex",
+    race ~ "Race",
+    ethnicity ~ "Ethnicity",
+    education ~ "Education",
+    has_insurance ~ "Has insurance", 
+    insurance ~ "Type of Insurance",
+    income_2021 ~ "Household income (2021 USD)",
+    totslf_2021 ~ "Total spending (2021 USD)"
+  ),
+  statistic = list(
+    AGE53X ~ "{mean} ({sd})",
+    income_2021 ~ "{median} ({p25}, {p75})",
+    totslf_2021 ~ "{median} ({p25}, {p75})",
+    all_categorical() ~ "{n} ({p}%)"
+  ),
+  missing = "ifany"
+)
+
+table1 <- add_p(table1)
+table1 <- bold_labels(table1)
+
+table1
