5f8108dc9dc7f59295f6a52c27b8e6ed7a2923c0
briantruan <86815054+briantruan@users.noreply.github.com>
2026-04-21 20:00:18 -0400

    this should be the linkage properly done

--- 06_regression.R
+++ 06_regression.R
@@ -87,3 +87,92 @@ add_reg <- svyglm(
   subset = adhd_any_rx == "Yes"
 )
 summary(add_reg)
+
+# 14. estimates
+svyby(
+  ~flag_adhd_dx,
+  ~year,
+  design = meps_design_adhd,
+  FUN = svytotal,
+  na.rm = TRUE,
+  vartype = c("se")
+)
+
+svyby(
+  ~AGE53X,
+  ~year,
+  design = meps_design_adhd,
+  FUN = svymean,
+  na.rm = TRUE,
+  vartype = c("se")
+)
+
+svyby(
+  ~adhd_fills + adhd_total_spend + adhd_oop + adhd_private + adhd_medicaid,
+  ~year,
+  design = meps_design_adhd_fills_T,
+  FUN = svymean,
+  na.rm = TRUE,
+  vartype = "se"
+)
+
+# mann-whitney u test
+svyranktest(adhd_fills ~ year, design = meps_design_adhd)
+svyranktest(adhd_total_spend ~ year, design = meps_design_adhd)
+svyranktest(adhd_oop ~ year, design = meps_design_adhd)
+svyranktest(adhd_private ~ year, design = meps_design_adhd)
+svyranktest(adhd_medicaid ~ year, design = meps_design_adhd)
+
+# variable of interest
+dep_var <- "adhd_oop_log"
+
+# glm
+modelt1 <- svyglm(as.formula(paste(dep_var, "~ post_covid")),
+                design = meps_design_adhd, family = quasipoisson())
+summary(model1)$coefficients
+
+# step-wise model with these vars: + sex + AGE53X + ethnicity + marital + education + has_insurance
+modelt2 <- svyglm(as.formula(paste(dep_var, "~ post_covid + sex")),
+                design = meps_design_adhd, family = quasipoisson())
+summary(modelt2)$coefficients
+
+modelt3 <- svyglm(as.formula(paste(dep_var, "~ post_covid + AGE53X")),
+                design = meps_design_adhd, family = quasipoisson())
+summary(modelt3)$coefficients
+
+modelt4 <- svyglm(as.formula(paste(dep_var, "~ post_covid + ethnicity")),
+                design = meps_design_adhd, family = quasipoisson())
+summary(modelt4)$coefficients
+
+modelt5 <- svyglm(as.formula(paste(dep_var, "~ post_covid + marital")),
+                design = meps_design_adhd, family = quasipoisson())
+summary(modelt5)$coefficients
+
+modelt6 <- svyglm(as.formula(paste(dep_var, "~ post_covid + education")),
+                design = meps_design_adhd, family = quasipoisson())
+summary(modelt6)$coefficients
+
+modelt7 <- svyglm(as.formula(paste(dep_var, "~ post_covid + has_insurance")),
+                design = meps_design_adhd, family = quasipoisson())
+summary(modelt7)$coefficients
+
+modelt8 <- svyglm(as.formula(paste(dep_var, "~ post_covid + income_2021")),
+                design = meps_design_adhd, family = quasipoisson())
+summary(modelt8)$coefficients
+
+# ---- MODEL BUILDING ----
+modelf1 <- svyglm(as.formula(paste(dep_var, "~ post_covid + has_insurance")),
+                design = meps_design_adhd, family = quasipoisson())
+summary(modelf1)$coefficients
+
+modelf2 <- svyglm(as.formula(paste(dep_var, "~ post_covid + has_insurance + income_2021")),
+                design = meps_design_adhd, family = quasipoisson())
+summary(modelf2)$coefficients
+
+modelf3 <- svyglm(as.formula(paste(dep_var, "~ post_covid + has_insurance + marital + AGE53X")),
+                design = meps_design_adhd, family = quasipoisson())
+summary(modelf3)$coefficients
+
+modelf4 <- svyglm(as.formula(paste(dep_var, "~ post_covid + has_insurance + marital + AGE53X + education")),
+                design = meps_design_adhd, family = quasipoisson())
+summary(modelf4)$coefficients
--- 07_clink.R
+++ 07_clink.R
@@ -10,7 +10,7 @@ rx_2021 <- rx_2021 %>% rename(EVNTIDX = LINKIDX)
 
 # 2. subset to ADHD only
 adhd_2019 <- cond_2019 %>% 
-  filter(str_detect(ICD10CDX, "^F90"))
+  filter(str_detect(ICD10CDX, "^F90")) 
 adhd_2021 <- cond_2021 %>% 
   filter(str_detect(ICD10CDX, "^F90"))
 
@@ -22,21 +22,32 @@ clnk_2021 <- adhd_2021 %>%
 
 # 4. remove dupes
 clnk_distinct_2019 <- clnk_2019 %>% 
-  distinct(DUPERSID, EVNTIDX, ICD10CDX, CCSR1X, EVENTYPE)
+  distinct(DUPERSID, EVNTIDX, ICD10CDX, CCSR1X, EVENTYPE, CONDIDX)
 clnk_distinct_2021 <- clnk_2021 %>% 
-  distinct(DUPERSID, EVNTIDX, ICD10CDX, CCSR1X, EVENTYPE)
+  distinct(DUPERSID, EVNTIDX, ICD10CDX, CCSR1X, EVENTYPE, CONDIDX)
 
-# QC: should be 0 duplicate EVNTIDX
-  if (clnk_distinct_2019 %>% pull(EVNTIDX) %>% duplicated() %>% sum() == 0) {
-    message("No duplicate EVNTIDX in 2019 CLINK data")
-  } else {
-    warning("Duplicate EVNTIDX found in 2019 CLINK data")
-  }
-  if (clnk_distinct_2021 %>% pull(EVNTIDX) %>% duplicated() %>% sum() == 0) {
-    message("No duplicate EVNTIDX in 2021 CLINK data")
-  } else {
-    warning("Duplicate EVNTIDX found in 2021 CLINK data")
-  }
+# QC: should be 0 duplicate DUPERSID + EVNTIDX + CCSR1X + CONDIDX
+if (
+  clnk_distinct_2019 %>% 
+    select(DUPERSID, EVNTIDX, CCSR1X, CONDIDX) %>% 
+    duplicated() %>% 
+    sum() == 0
+) {
+  message("No duplicate DUPERSID + EVNTIDX + CCSR1X + CONDIDX in 2019 CLINK data")
+} else {
+  warning("Duplicate DUPERSID + EVNTIDX + CCSR1X + CONDIDX found in 2019 CLINK data")
+}
+
+if (
+  clnk_distinct_2021 %>% 
+    select(DUPERSID, EVNTIDX, CCSR1X, CONDIDX) %>% 
+    duplicated() %>% 
+    sum() == 0
+) {
+  message("No duplicate DUPERSID + EVNTIDX + CCSR1X + CONDIDX in 2021 CLINK data")
+} else {
+  warning("Duplicate DUPERSID + EVNTIDX + CCSR1X + CONDIDX found in 2021 CLINK data")
+}
 
 # 5. link clnk_distinct to rx
 adhd_rx_merged_2019 <- clnk_distinct_2019 %>% 
@@ -47,9 +58,11 @@ adhd_rx_merged_2021 <- clnk_distinct_2021 %>%
 # QC: check that EVENTYPE = 8 PRESCRIBED MEDICINE for all rows
 adhd_rx_merged_2019 %>% 
   count(EVENTYPE)
+nrow(adhd_rx_merged_2019)
 
 adhd_rx_merged_2021 %>% 
   count(EVENTYPE)
+nrow(adhd_rx_merged_2021)
 
 # 6. roll up to person-level data
 
@@ -57,10 +70,10 @@ adhd_2019_by_person <- adhd_rx_merged_2019 %>%
   group_by(DUPERSID) %>% 
   summarize(
     adhd_fills = n_distinct(RXRECIDX),
-    adhd_total_spend = sum(RXXP),
-    adhd_oop = sum(RXSF),
-    adhd_private = sum(RXPV),
-    adhd_medicaid = sum(RXMD)) %>% 
+    adhd_total_spend = sum(RXXP, na.rm = TRUE),
+    adhd_oop = sum(RXSF, na.rm = TRUE),
+    adhd_private = sum(RXPV, na.rm = TRUE),
+    adhd_medicaid = sum(RXMD, na.rm = TRUE)) %>% 
   mutate(adhd_pmed_flag = 1) %>% 
   mutate(oop_share = if_else(adhd_total_spend > 0, adhd_oop / adhd_total_spend * 100, NA_real_))
 
@@ -68,10 +81,10 @@ adhd_2021_by_person <- adhd_rx_merged_2021 %>%
   group_by(DUPERSID) %>% 
   summarize(
     adhd_fills = n_distinct(RXRECIDX),
-    adhd_total_spend = sum(RXXP),
-    adhd_oop = sum(RXSF),
-    adhd_private = sum(RXPV),
-    adhd_medicaid = sum(RXMD)) %>% 
+    adhd_total_spend = sum(RXXP, na.rm = TRUE),
+    adhd_oop = sum(RXSF, na.rm = TRUE),
+    adhd_private = sum(RXPV, na.rm = TRUE),
+    adhd_medicaid = sum(RXMD, na.rm = TRUE)) %>% 
   mutate(adhd_pmed_flag = 1) %>% 
   mutate(oop_share = if_else(adhd_total_spend > 0, adhd_oop / adhd_total_spend * 100, NA_real_))
 
@@ -106,52 +119,64 @@ nrow(adhd_2021_by_person)
 fyc_adhd_2019 <- fyc_adhd_2019 %>% mutate(year = 2019)
 fyc_adhd_2021 <- fyc_adhd_2021 %>% mutate(year = 2021)
 
-# final cleaned df
-fyc_adhd_clean <- bind_rows(fyc_adhd_2019, fyc_adhd_2021)
+# 10. keep persons with data in both years only
+ids_both <- intersect(fyc_adhd_2019$DUPERSID, fyc_adhd_2021$DUPERSID)
+fyc_adhd_2019 <- fyc_adhd_2019 %>% filter(DUPERSID %in% ids_both)
+fyc_adhd_2021 <- fyc_adhd_2021 %>% filter(DUPERSID %in% ids_both)
 
-# 10. define design variable
+# 11. flag for ADHD in either year using cond files
+adhd_cond_2019 <- cond_2019 %>% 
+  filter(str_detect(ICD10CDX, "^F90")) %>% 
+  distinct(DUPERSID) %>% 
+  mutate(flag_adhd_dx = 1)
+adhd_cond_2021 <- cond_2021 %>% 
+  filter(str_detect(ICD10CDX, "^F90")) %>% 
+  distinct(DUPERSID) %>% 
+  mutate(flag_adhd_dx = 1)
+fyc_adhd_2019 <- fyc_adhd_2019 %>% 
+  left_join(adhd_cond_2019, by = "DUPERSID")
+fyc_adhd_2021 <- fyc_adhd_2021 %>% 
+  left_join(adhd_cond_2021, by = "DUPERSID")
+fyc_adhd_2019 <- fyc_adhd_2019 %>% 
+  mutate(flag_adhd_dx = replace_na(flag_adhd_dx, 0))
+fyc_adhd_2021 <- fyc_adhd_2021 %>% 
+  mutate(flag_adhd_dx = replace_na(flag_adhd_dx, 0))
+
+# 12. final cleaned df
+fyc_clean <- bind_rows(fyc_adhd_2019, fyc_adhd_2021)
+
+# count num of adhd_pmed_flag vs. flag_adhd_dx
+fyc_clean %>% count(adhd_pmed_flag, flag_adhd_dx)
+
+# per shrestha's recommendation, we should log-transform spend vars
+# also add binary indicator for post-covid (2021)
+fyc_clean <- fyc_clean %>% 
+  mutate(
+    post_covid = if_else(year == 2021, 1, 0),
+    adhd_total_spend_log = log(adhd_total_spend + 1),
+    adhd_oop_log = log(adhd_oop + 1),
+    adhd_private_log = log(adhd_private + 1),
+    adhd_medicaid_log = log(adhd_medicaid + 1)
+  )
+
+# 13. define design variable
+
+options(survey.lonely.psu = "adjust")
+
+# this has everyone
 meps_design <- svydesign(
   id = ~VARPSU,
   strata = ~VARSTR,
   weights = ~PERWT,
-  data = fyc_adhd_clean,
+  data = fyc_clean,
   nest = TRUE
 )
 
-meps_design_adhd <- subset(meps_design, adhd_pmed_flag == 1) 
-meps_design_adhd_2019 <- subset(meps_design_adhd, year == 2019)
-meps_design_adhd_2021 <- subset(meps_design_adhd, year == 2021)
+# subset to adhd only
+meps_design_adhd <- subset(meps_design, flag_adhd_dx == 1)
 
-# Make a binary factor for year and run svyttest
-meps_design_adhd <- update(meps_design_adhd,
-  year_bin = factor(ifelse(year == 2021, "2021", "2019"))
-)
+# subset to adhd only and with fills
+meps_design_adhd_fills_T <- subset(meps_design, flag_adhd_dx == 1 & adhd_pmed_flag == 1)
 
-# 11. estimates
-svymean(~ adhd_fills +
-          adhd_total_spend +
-          adhd_oop +
-          adhd_private +
-          adhd_medicaid,
-          design = meps_design_adhd_2019)
-
-svymean(~ adhd_fills +
-          adhd_total_spend +
-          adhd_oop +
-          adhd_private +
-          adhd_medicaid,
-          design = meps_design_adhd_2021)
-
-# hist to evaluate normality
-svyhist(~ adhd_fills, design = meps_design_adhd)
-svyhist(~ adhd_total_spend, design = meps_design_adhd)
-svyhist(~ adhd_oop, design = meps_design_adhd)
-svyhist(~ adhd_private, design = meps_design_adhd)
-svyhist(~ adhd_medicaid, design = meps_design_adhd)
-
-# mann-whitney u test
-svyranktest(adhd_fills ~ year_bin, design = meps_design_adhd)
-svyranktest(adhd_total_spend ~ year_bin, design = meps_design_adhd)
-svyranktest(adhd_oop ~ year_bin, design = meps_design_adhd)
-svyranktest(adhd_private ~ year_bin, design = meps_design_adhd)
-svyranktest(adhd_medicaid ~ year_bin, design = meps_design_adhd)
+# subset to adhd only and with no fills
+meps_design_adhd_fills_F <- subset(meps_design, flag_adhd_dx == 1 & adhd_pmed_flag == 0)
